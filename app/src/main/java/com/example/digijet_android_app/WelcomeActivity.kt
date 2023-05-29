740965beb02a1748402136077583bfc2209ae727
Angelika Grebenik <likysagrebenik@gmail.com>
2023-05-30 01:16:10 +0300

    welcomeActivity design

--- app/src/main/java/com/example/digijet_android_app/WelcomeActivity.kt
+++ app/src/main/java/com/example/digijet_android_app/WelcomeActivity.kt
@@ -2,8 +2,12 @@ package com.example.digijet_android_app
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 class WelcomeActivity : AppCompatActivity() {
 
@@ -13,17 +17,33 @@ class WelcomeActivity : AppCompatActivity() {
 
         val loginButton: Button = findViewById(R.id.loginButton)
         val registerButton: Button = findViewById(R.id.registerButton)
+        val logoImageView: ImageView = findViewById(R.id.logoImageView)
 
-        loginButton.setOnClickListener {
-            // Переадресация на активити для логина
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
+        Glide.with(this)
+            .load(R.drawable.logo_image)
+            .apply(RequestOptions().transform(RoundedCorners(40)))
+            .into(logoImageView)
 
         registerButton.setOnClickListener {
-            // Переадресация на активити для регистрации
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
+            // Здесь можно выполнить действия перед переходом к регистрации
+            navigateToRegistration()
         }
+
+        loginButton.setOnClickListener {
+            // Здесь можно выполнить действия перед переходом к входу в систему
+            navigateToLogin()
+        }
+    }
+
+    private fun navigateToRegistration() {
+        // Запуск страницы загрузки перед переходом к регистрации
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToLogin() {
+        // Запуск страницы загрузки перед переходом к входу в систему
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
