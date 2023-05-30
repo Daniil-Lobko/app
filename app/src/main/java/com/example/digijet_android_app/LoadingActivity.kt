92381df30f5f2d400f463f918343df1df754568e
Angelika Grebenik <likysagrebenik@gmail.com>
2023-05-30 18:20:51 +0300

    update LoadingPage

--- app/src/main/java/com/example/digijet_android_app/LoadingActivity.kt
+++ app/src/main/java/com/example/digijet_android_app/LoadingActivity.kt
@@ -9,32 +9,19 @@ import androidx.appcompat.app.AppCompatActivity
 
 class LoadingActivity : AppCompatActivity() {
 
-    private val loadingTimeMillis: Long = 3000 // Время загрузки в миллисекундах
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
 
         val logoImageView: ImageView = findViewById(R.id.logoImageView)
-        val progressBar: ProgressBar = findViewById(R.id.progressBar)
 
-        // Загрузка логотипа в ImageView
-        logoImageView.setImageResource(R.drawable.logo_image)
+        logoImageView.alpha =0f
+        logoImageView.animate().setDuration(2000).alpha(1f).withEndAction{
+            val i = Intent(this, WelcomeActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
 
-        // Установка черного цвета для ProgressBar
-        progressBar.indeterminateDrawable.setColorFilter(
-            resources.getColor(android.R.color.black),
-            android.graphics.PorterDuff.Mode.SRC_IN
-        )
-
-        // Здесь можно выполнить необходимые операции загрузки данных или выполнить асинхронные операции
-
-        // Создаем задержку с использованием Handler
-        Handler().postDelayed({
-            // Переходим к WelcomeActivity
-            val intent = Intent(this@LoadingActivity, WelcomeActivity::class.java)
-            startActivity(intent)
-            finish() // Закрываем LoadingActivity после перехода
-        }, loadingTimeMillis)
     }
 }
